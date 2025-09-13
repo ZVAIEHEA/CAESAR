cc6382f5e0021d1624146b3ceaf2f450d865be39
ZVAIEHEA <paulrvr2005@gmail.com>
2025-09-13 19:08:00 +0200

    test

--- 02_Process/01_main.py
+++ 02_Process/01_main.py
@@ -3,9 +3,16 @@ import importlib
 data = importlib.import_module('02_data')
 
 data = data.data
+<<<<<<< HEAD
 weather_data = data.weather_data
 
 if __name__ == "__main__":
     file_path = '01_Input/Global-Oil-and-Gas-Extraction-Tracker-Feb-2025.xlsx'
     data(file_path)
     weather_data(file_path)
+=======
+
+if __name__ == "__main__":
+    file_path = '01_Input/Global-Oil-and-Gas-Extraction-Tracker-Feb-2025.xlsx'
+    data(file_path)
+>>>>>>> df364a0 (test)
--- 02_Process/02_data.py
+++ 02_Process/02_data.py
@@ -3,6 +3,11 @@ import openmeteo_requests
 import pandas as pd
 import requests_cache
 from retry_requests import retry
+<<<<<<< HEAD
+=======
+import time
+
+>>>>>>> df364a0 (test)
 
 
 #       Use this script to download weather data for every location on the offshore station dataset that i will provide
@@ -12,6 +17,7 @@ from retry_requests import retry
 #       and the Oil Market is it on (Brent, WTI, etc)
 
 def data(file_path):
+<<<<<<< HEAD
     df =  pd.read_excel(file_path, sheet_name='Main data')
     df = df[df['Onshore/Offshore'] == 'Offshore']
 
@@ -49,3 +55,102 @@ async def weather_data(latitude, longitude):
 	print(f"Current time: {current.Time()}")
 	print(f"Current temperature_2m: {current_temperature_2m}")
 	print(f"Current relative_humidity_2m: {current_relative_humidity_2m}")
+=======
+	df =  pd.read_excel(file_path, sheet_name='Main data')
+	df = df[df['Onshore/Offshore'] == 'Offshore']
+
+	model_df = df[['Operator','Unit Name','Country/Area','Latitude', 'Longitude','Onshore/Offshore']].dropna()
+
+	date_debut = '2024-01-01'
+	date_fin = '2024-01-31'
+	dates = pd.date_range(start=date_debut, end=date_fin, freq='D')
+	#print(dates)
+
+	for date in dates:
+		print(f"Récupération des données météo pour le {date.strftime('%Y-%m-%d')}")
+		print(date)
+		date = date.strftime('%Y-%m-%d')
+		print(date)
+		for rows in model_df.iterrows():
+			latitude = rows[1]['Latitude']
+			longitude = rows[1]['Longitude']
+			hourly_data = historical_weather_api(latitude, longitude, date)
+			time.sleep(0.1) 
+			all_hourly_data = ["temperature_2m", "relative_humidity_2m", "rain", "pressure_msl", "surface_pressure", "et0_fao_evapotranspiration", "vapour_pressure_deficit", "wind_speed_100m", "wind_speed_10m", "wind_gusts_10m", "shortwave_radiation", "terrestrial_radiation"]
+			for data in all_hourly_data:
+				model_df[data] = hourly_data[data]
+
+
+		print(model_df)
+
+
+
+# Make sure that to use the marine API, Weather API, and other Open Meteo APIs
+def historical_weather_api(latitude, longitude, date):
+	# Setup the Open-Meteo API client with cache and retry on error
+	cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+	retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+	openmeteo = openmeteo_requests.Client(session = retry_session)
+
+	# Make sure all required weather variables are listed here
+	# The order of variables in hourly or daily is important to assign them correctly below
+	url = "https://archive-api.open-meteo.com/v1/archive"
+	params = {
+		"latitude": latitude,
+		"longitude": longitude,
+		"start_date": date,
+		"end_date": date,
+		"hourly": ["temperature_2m", "relative_humidity_2m", "rain", "pressure_msl", "surface_pressure", "et0_fao_evapotranspiration", "vapour_pressure_deficit", "wind_speed_100m", "wind_speed_10m", "wind_gusts_10m", "shortwave_radiation", "terrestrial_radiation"],
+		"models": ["era5_land", "era5_ensemble"],
+	}
+	responses = openmeteo.weather_api(url, params=params)
+
+	# Process 1 location and 2 models
+	for response in responses:
+		print(f"\nCoordinates: {response.Latitude()}°N {response.Longitude()}°E")
+		print(f"Elevation: {response.Elevation()} m asl")
+		print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
+		print(f"Model Nº: {response.Model()}")
+		
+		# Process hourly data. The order of variables needs to be the same as requested.
+		hourly = response.Hourly()
+		hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+		hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+		hourly_rain = hourly.Variables(2).ValuesAsNumpy()
+		hourly_pressure_msl = hourly.Variables(3).ValuesAsNumpy()
+		hourly_surface_pressure = hourly.Variables(4).ValuesAsNumpy()
+		hourly_et0_fao_evapotranspiration = hourly.Variables(5).ValuesAsNumpy()
+		hourly_vapour_pressure_deficit = hourly.Variables(6).ValuesAsNumpy()
+		hourly_wind_speed_100m = hourly.Variables(7).ValuesAsNumpy()
+		hourly_wind_speed_10m = hourly.Variables(8).ValuesAsNumpy()
+		hourly_wind_gusts_10m = hourly.Variables(9).ValuesAsNumpy()
+		hourly_shortwave_radiation = hourly.Variables(10).ValuesAsNumpy()
+		hourly_terrestrial_radiation = hourly.Variables(11).ValuesAsNumpy()
+
+		hourly_data = {"date": pd.date_range(
+			start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+			end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+			freq = pd.Timedelta(seconds = hourly.Interval()),
+			inclusive = "left"
+		)}
+		
+		hourly_data["temperature_2m"] = hourly_temperature_2m
+		hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+		hourly_data["rain"] = hourly_rain
+		hourly_data["pressure_msl"] = hourly_pressure_msl
+		hourly_data["surface_pressure"] = hourly_surface_pressure
+		hourly_data["et0_fao_evapotranspiration"] = hourly_et0_fao_evapotranspiration
+		hourly_data["vapour_pressure_deficit"] = hourly_vapour_pressure_deficit
+		hourly_data["wind_speed_100m"] = hourly_wind_speed_100m
+		hourly_data["wind_speed_10m"] = hourly_wind_speed_10m
+		hourly_data["wind_gusts_10m"] = hourly_wind_gusts_10m
+		hourly_data["shortwave_radiation"] = hourly_shortwave_radiation
+		hourly_data["terrestrial_radiation"] = hourly_terrestrial_radiation
+		
+
+		return hourly_data
+		# hourly_dataframe = pd.DataFrame(data = hourly_data)
+		# print("\nHourly data\n", hourly_dataframe)
+		
+		
+>>>>>>> df364a0 (test)
